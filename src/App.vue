8faf4956d0787824fc3608a61a320f993b1a5b48
Nicholas Fung <fdq68@outlook.com>
2022-01-07 16:34:56 +0800

    增加上传进度条

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,34 @@
 <template>
   <div id="app">
-    <input type="file" @change="handleFileChange">
-    <el-button @click="handleUpload">上传</el-button>
+    <h1>文件上传demo</h1>
+
+    <section>
+      <h2>上传文件</h2>
+      <input type="file" @change="handleFileChange">
+      <el-button @click="handleUpload">上传</el-button>
+    </section>
+
+    <section class="total-upload-progress">
+      <h2>上传总进度</h2>
+      <el-progress :percentage="fakeUploadPercentage"></el-progress>
+    </section>
+
+    <section class="slice-upload-progress">
+      <h2>切片上传进度</h2>
+      <el-table :data="data">
+        <el-table-column prop="hash" label="切片hash" align="center"></el-table-column>
+        <el-table-column label="大小(KB)" align="center" width="120">
+          <template v-slot="{ row }">
+            {{ row.size | transformByte }}
+          </template>
+        </el-table-column>
+        <el-table-column label="进度" align="center">
+          <template v-slot="{ row }">
+            <el-progress :percentage="row.percentage" color="#909399"></el-progress>
+          </template>
+        </el-table-column>
+      </el-table>
+    </section>
   </div>
 </template>
 
@@ -15,7 +42,29 @@ export default {
       container: {
         file: null
       },
-      data: []
+      data: [],
+      fakeUploadPercentage: 0
+    }
+  },
+  filters: {
+    transformByte(val) {
+      return Number((val / 1024).toFixed(0))
+    }
+  },
+  computed: {
+    uploadPercentage() {
+      if (!this.container.file || !this.data.length) {
+        return 0
+      }
+      const loaded = this.data.map(item => item.size * item.percentage).reduce((acc, curt) => acc + curt)
+      return parseInt((loaded / this.container.file.size).toFixed(2))
+    }
+  },
+  watch: {
+    uploadPercentage(now) {
+      if (now > this.fakeUploadPercentage) {
+        this.fakeUploadPercentage = now
+      }
     }
   },
   methods: {
@@ -24,10 +73,12 @@ export default {
       method = 'post',
       data,
       headers = {},
+      onProgress = p => p,
       // requestList
     }) {
       return new Promise(resolve => {
         const xhr = new XMLHttpRequest()
+        xhr.upload.onprogress = onProgress // XMLHttpRequest原生支持上传进度条监听，只需要监听upload.progress即可
         xhr.open(method, url)
         Object.keys(headers).forEach(key => {
           xhr.setRequestHeader(key, headers[key])
@@ -59,19 +110,20 @@ export default {
     // 上传切片，同时过滤已上传的切片
     async uploadChunks() {
       const requestList = this.data
-        .map(({ chunk, hash }) => {
+        .map(({ chunk, hash, index }) => {
           // 处理formData
           const formData = new FormData()
           formData.append('chunk', chunk)
           formData.append('hash', hash)
           formData.append('filename', this.container.file.name)
-          return { formData }
+          return { formData, index }
         })
-        .map(async ({ formData }) => {
+        .map(async ({ formData, index }) => {
           // 分片上传
           this.request({
             url: 'http://localhost:3001',
-            data: formData
+            data: formData,
+            onProgress: this.createProgressHandler(this.data[index])
           })
         })
       await Promise.all(requestList)
@@ -91,13 +143,23 @@ export default {
       this.$message.success('上传成功')
     },
     async handleUpload() {
-      if (!this.container.file) return
+      if (!this.container.file) {
+        return this.$message.warning('未选择文件！')
+      }
       const fileChunkList = this.createFileChunk(this.container.file)
       this.data = fileChunkList.map(({ file }, index) => ({
+        index,
+        percentage: 0,
+        size: file.size,
         chunk: file,
         hash: this.container.file.name + '-' + index // 文件名 + 数组下标
       }))
       await this.uploadChunks()
+    },
+    createProgressHandler(item) {
+      return p => {
+        item.percentage = parseInt(String((p.loaded / p.total) * 100))
+      }
     }
   }
 }
@@ -105,11 +167,14 @@ export default {
 
 <style>
 #app {
+  padding: 20px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.total-upload-progress,
+.slice-upload-progress {
+  padding-top: 20px;
 }
 </style>
